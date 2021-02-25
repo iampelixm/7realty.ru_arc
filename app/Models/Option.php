e251a5b35518094a8595cff977755333f9f6b666
iampelixm <pelixm@ya.ru>
2021-02-25 12:09:04 +0000

    fillable slug

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -8,32 +8,33 @@ class Option extends Model
 {
     public $timestamps = false;
     protected $table = 'options';
-    protected $fillable = ['name', 'active', 'values', 'method_input', 'require'];
+    protected $fillable = ['name', 'active', 'values', 'method_input', 'require', 'slug'];
 
     public function getValueCountAttribute()
     {
-    	if ($this->values != null){
-    		$values = (array) json_decode($this->values);
-    		$count = count($values);
-    	} else {
-    		$count = 0;
-    	}
-    	return $count;
+        if ($this->values != null) {
+            $values = (array) json_decode($this->values);
+            $count = count($values);
+        } else {
+            $count = 0;
+        }
+        return $count;
     }
 
-    public function types() {
+    public function types()
+    {
         return $this->hasManyThrough(
             'App\Models\Type',
-            'App\Models\OptionType', 
+            'App\Models\OptionType',
             'option_id',
             'id',
             'id',
-            'type_id');
+            'type_id'
+        );
     }
 
     public function typesId()
     {
         return $this->hasMany('App\Models\OptionType', 'option_id', 'id');
     }
-
 }
