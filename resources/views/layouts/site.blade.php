0151ebad58c60abece0555227e1068ca07d09e33
iampelixm <pelixm@ya.ru>
2021-03-30 12:41:51 +0000

    added metric

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -72,7 +72,7 @@
         })
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(73375960, "init", {
+        ym(73798639, "init", {
             clickmap: true,
             trackLinks: true,
             accurateTrackBounce: true,
@@ -81,7 +81,7 @@
 
     </script>
     <noscript>
-        <div><img src="https://mc.yandex.ru/watch/73375960" style="position:absolute; left:-9999px;" alt="" /></div>
+        <div><img src="https://mc.yandex.ru/watch/73798639" style="position:absolute; left:-9999px;" alt="" /></div>
     </noscript>
     <!-- /Yandex.Metrika counter -->
 </body>
