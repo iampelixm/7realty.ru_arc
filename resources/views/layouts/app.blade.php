46d13db4c6d9a4ffa7bad4c5f9d8f0ae2b42629c
iampelixm <pelixm@ya.ru>
2021-11-09 07:25:33 +0000

    we

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <meta name="theme-color" content="#C1A771">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
